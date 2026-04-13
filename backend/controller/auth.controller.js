204c1d2b7a25c4cd2a8efa82da782c8e2f564acc
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-04-13 15:46:05 -0700

    fix cookie setup

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -38,7 +38,7 @@ export const signup = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // true in production (HTTPS)
+      secure: true, // true in production (HTTPS)
       sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
